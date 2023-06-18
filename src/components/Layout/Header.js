b6d582bb759636a0cda58998a4a3b932b3ce8073
CyberManMatt <mtthwjarrett0@gmail.com>
2023-06-18 11:44:45 -0400

    Added Cart Button - Added CartIcon.js to display cart icon
    - Added HeaderCartButton.js and HeaderCartButton.module.css to display cart button
    
    - Modified Header.js to include the HeaderCartButton component
    
    - Added README.md to components folder for components documentation

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,18 @@
 /*
 Component Type: Presentation
-
+Description: Webpage Header
 */
 
 import headerImage from "../../assets/meals.jpg"
 import classes from "./Header.module.css"
+import {HeaderCartButton} from "./HeaderCartButton";
 
 export function Header(props) {
     return (
         <>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes["main-image"]}>
                 <img src={headerImage} alt="table of food"/>
---
+++ src/components/README.md
@@ -0,0 +1,6 @@
+# Component Types
+Presentation: No logic is implemented to this type of component. Just a 
+```return``` statement that returns JSX.
+
+Logic: Component has logic such as getting data, state, and other logic is
+implemented into the component. The component still returns JSX.
