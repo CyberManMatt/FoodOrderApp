b7c6fd30c624705032ba3d858559d436be901fcf
Matt Jarrett <mtthwjarrett0@gmail.com>
2023-06-22 17:50:02 +0000

    Added comments

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,15 @@
+/*
+Component Type: Logic
+Description: Cart modal that appears when the user clicks the Cart button in the header. Cart contents is
+dynamic.
+*/
+
 import classes from "./Cart.module.css"
 import {Modal} from "../UI/Modal";
 
 export function Cart(props) {
+
+    // Initialize cart items content
     const cartItems = <ul className={classes["cart-items"]}>{[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(i => <li>{i.name}</li>)}</ul>
 
     return (
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,12 +1,20 @@
+/*
+Component Type: Presentation
+Description: Reuseable modal component with a backdrop that is rendered in the overlays portal in index.html
+*/
+
 import classes from "./Modal.module.css"
 import ReactDOM from "react-dom"
 
+// Initialize portal element
 const portal = document.getElementById("overlays")
 
+// Initialize Backdrop component to be used in the Modal component
 const Backdrop = (props) => {
     return <div className={classes.backdrop} />
 }
 
+// Initialize Modal Overlay component to be used in the Modal component
 const ModalOverlay = (props) => {
     return (
         <div className={classes.modal}>
@@ -18,6 +26,7 @@ const ModalOverlay = (props) => {
 export function Modal(props) {
     return (
         <>
+            {/* Create portals for the Backdrop and Modal Overlay in the "overlays" id in index.html */}
             {ReactDOM.createPortal(<Backdrop />, portal)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portal)}
         </>
